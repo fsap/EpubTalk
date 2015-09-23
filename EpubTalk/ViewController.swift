d383016e423b7707f4d9408dbc8274c90c06d0ea
Fujiwara <dev.fsap@gmail.com>
2015-09-23 22:41:14 +0900

    Initial Commit

---
+++ EpubTalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EpubTalk
+//
+//  Created by Fujiwara on 2015/09/23.
+//  Copyright (c) 2015年 FSAP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
